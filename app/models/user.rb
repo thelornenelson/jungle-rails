79c4481b9295fd23aa9944fcfe8d04b8f895092a
Andrew Barclay <andrewkbarclay@gmail.com>
2018-04-26 19:53:28 +0000

    added validation for user registration

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,7 @@ class User < ActiveRecord::Base
 
   validates :first_name, :last_name, :email, presence: true
   validates :email, uniqueness: true
+  validates :password, confirmation: true
 
   has_secure_password
 
