4be8ecba4cfdc10324cf7ab10e5dcb01f2c93cd5
Andrew Barclay <andrewkbarclay@gmail.com>
2018-04-27 18:16:09 +0000

    added review migration and model validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,7 @@
 class User < ActiveRecord::Base
 
+  has_many :reviews
+
   validates :first_name, :last_name, :email, presence: true
   validates :email, uniqueness: true
   validates :password, confirmation: true
