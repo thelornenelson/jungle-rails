840bf86c94ecf70b4814a624d6e3c5ab40d01ff3
Andrew Barclay <andrewkbarclay@gmail.com>
2018-04-30 18:38:21 +0000

    added product model tests

---
+++ spec/models/product_spec.rb
@@ -0,0 +1,40 @@
+require 'rails_helper'
+
+RSpec.describe Product, type: :model do
+  describe 'Validations' do
+    subject(:category) { Category.new name: 'test' }
+    subject(:product) { category.products.new({
+      name: 'test',
+      description: 'test description',
+      image: 'image.jpg',
+      price_cents: 4500,
+      quantity: 50
+      })
+    }
+
+    it 'should be valid when name, description, image, price, quantity, and category are included' do
+        expect(product).to be_valid
+    end
+
+    it 'should not be valid when name is nil' do
+      product.name = nil
+      expect(product).not_to be_valid
+    end
+
+    it 'should not be valid when price is nil' do
+      product.price_cents = nil
+      expect(product).not_to be_valid
+    end
+
+    it 'should not be valid when quantity is nil' do
+      product.quantity = nil
+      expect(product).not_to be_valid
+    end
+
+    it 'should not be valid when category is nil' do
+      product.category = nil
+      expect(product).not_to be_valid
+    end
+
+  end
+end
