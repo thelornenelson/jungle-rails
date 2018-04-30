7c8db563d38527f84a48fcf066b3b278f83f7a62
Andrew Barclay <andrewkbarclay@gmail.com>
2018-04-30 18:46:33 +0000

    added expect tests for product validation errors

--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -19,21 +19,27 @@ RSpec.describe Product, type: :model do
     it 'should not be valid when name is nil' do
       product.name = nil
       expect(product).not_to be_valid
+      expect(product.errors.full_messages).to include "Name can't be blank"
     end
 
     it 'should not be valid when price is nil' do
       product.price_cents = nil
       expect(product).not_to be_valid
+      expect(product.errors.full_messages).to include "Price can't be blank"
+
     end
 
     it 'should not be valid when quantity is nil' do
       product.quantity = nil
       expect(product).not_to be_valid
+      expect(product.errors.full_messages).to include "Quantity can't be blank"
     end
 
     it 'should not be valid when category is nil' do
       product.category = nil
       expect(product).not_to be_valid
+      expect(product.errors.full_messages).to include "Category can't be blank"
+
     end
 
   end
