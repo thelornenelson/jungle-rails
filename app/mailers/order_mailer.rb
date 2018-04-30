4a76cc69bc8659ff5a308a738b3b6eb62d6254ca
Andrew Barclay <andrewkbarclay@gmail.com>
2018-04-30 21:15:19 +0000

    will only try to send confirmation email in development env

--- app/mailers/order_mailer.rb
+++ app/mailers/order_mailer.rb
@@ -3,8 +3,10 @@ class OrderMailer < ApplicationMailer
 
   def order_confirmation(order)
     @order = order
-    @url = 'http://localhost:3000'
-    mail(to: @order.email, subject: @order.id)
+    if Rails.env.development?
+      @url = 'http://localhost:3000'
+      mail(to: @order.email, subject: @order.id)
+    end
   end
 
 
