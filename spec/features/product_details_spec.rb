848fe7364449bcfb966230f76d39e7d4bbc10daf
Andrew Barclay <andrewkbarclay@gmail.com>
2018-05-01 19:02:25 +0000

    added test to check product details on product show page

---
+++ spec/features/product_details_spec.rb
@@ -0,0 +1,43 @@
+require 'rails_helper'
+
+RSpec.feature "ProductDetails", type: :feature, js: true do
+
+  before :each do
+    @category = Category.create! name: 'Apparel'
+
+    10.times do |n|
+      @category.products.create!(
+        name: Faker::Hipster.sentence(3),
+        description: Faker::Hipster.paragraph(4),
+        image: open_asset('apparel1.jpg'),
+        quantity: 10,
+        price: 64.99
+      )
+    end
+  end
+
+  scenario "They navigate to a product detail page from the home page by clicking on a product" do
+    visit root_path
+
+    # click on
+    first('article.product a').click
+
+    expect(page).to have_css 'section.products-show'
+
+    save_screenshot
+  end
+
+  scenario "They can view details for the correct product on the product detail page" do
+    visit root_path
+
+    product = Product.first
+
+    click_on product.name
+
+    expect(page).to have_content product.name
+    expect(page).to have_content product.description
+    expect(page).to have_content product.quantity
+    expect(page).to have_content product.price
+
+  end
+end
